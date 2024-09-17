5bea0a1f3356a08075a72f1848c11866dc0184d0
Александр Сминов ПрИ-202 <smirnovalexandr234@gmail.com>
2024-09-17 21:27:05 +0500

    Tasks 10 and 11

--- task10/src/com/example/task10/Task10.java
+++ task10/src/com/example/task10/Task10.java
@@ -1,6 +1,7 @@
 package com.example.task10;
 
-import static java.lang.Float.isNaN;
+import static java.lang.Float.*;
+import static java.lang.System.out;
 
 public class Task10 {
 
@@ -9,14 +10,18 @@ public class Task10 {
         // TODO корректно сравнивать два значения типа float с заданной пользователем точностью (параметр - количество знаков после запятой).
         // Функция должна корректно обрабатывать ситуацию со сравнением значений бесконечности.
         // Функция должна считать значения «не число» NaN (например 0.0/0.0) равными между собой.
-        a = (float) (Math.round(a * Math.pow( 10, precision)) / Math.pow( 10, precision));
-        b = (float) (Math.round(b * Math.pow( 10, precision)) / Math.pow( 10, precision));
 
-        boolean result = (a == b);
-        if (isNaN(a) && isNaN(b)) result = true;
+        if (isNaN(a) && isNaN(b)) return true;
+        if (isNaN(a) || isNaN(b)) return false;
+        if (a == POSITIVE_INFINITY && b == POSITIVE_INFINITY || a == NEGATIVE_INFINITY && b == NEGATIVE_INFINITY) return true;
+        if (isInfinite(a) || isInfinite(b)) return false;
 
-        return result;
+        return Math.abs(a - b) < (float) Math.pow(0.1, precision);
 
+
+//        a = (float) Math.abs(a * Math.pow( 10, precision));
+//        b = (float) Math.abs(b * Math.pow( 10, precision));
+//        out.println("``````````````````````````````````````  " + a + " - " + b + " = " + (a-b));
     }
 
     public static void main(String[] args) {
@@ -26,7 +31,7 @@ public class Task10 {
         float c = 0.7f;
 
         boolean result = compare(sum, c, 2);
-        System.out.println(result);
+        out.println(result);
 
     }
 
